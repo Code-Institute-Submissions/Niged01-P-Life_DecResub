d32119aea51f9c44898e22886c694c71c94f3b09
Niged01 <nigedoberer@gmail.com>
2022-12-15 11:07:16 +0000

    Refactored Profiles Template

--- forum/urls.py
+++ forum/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
